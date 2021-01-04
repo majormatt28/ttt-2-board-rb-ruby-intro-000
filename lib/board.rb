ff5d2157557bbc287d798f232210cf51158a5b80
Mathew E Newton <elijah2895@hotmail.com>
2021-01-04 23:23:08 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,11 @@
 # Define the variable board below.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+row = [0]
+row = [1]
+row = [2]
+row = [3]
+row = [4]
+row = [5]
+row = [6]
+row = [7]
+row = [8]
